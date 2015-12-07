9cb97087a01ef0c994bbb951dfe56f1f08298b95
Jason Shepherd <jshepherd@outlook.com>
2015-12-07 10:14:59 -0700

    Main menu update with panel fragments and shared preferences.

--- app/src/main/java/edu/slcc/jasonshepherd/jasonshepherdhangdroid/SidePanel.java
+++ app/src/main/java/edu/slcc/jasonshepherd/jasonshepherdhangdroid/SidePanel.java
@@ -1,11 +1,14 @@
 package edu.slcc.jasonshepherd.jasonshepherdhangdroid;
 
 import android.app.Fragment;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import edu.slcc.jasonshepherd.jasonshepherdhangdroid.R;
 
@@ -16,6 +19,27 @@ public class SidePanel extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_high_scores, container, false);
+
+        View v = inflater.inflate(R.layout.side_fragment, container, false);
+
+
+        // access the shared preferences file to get the high score and name
+        SharedPreferences hangDroidPrefs = this.getActivity().getSharedPreferences("JSHangDroidPrefs", Context.MODE_PRIVATE);
+        String nameData = hangDroidPrefs.getString("PLAYER_NAME_KEY", "Default");
+        int pointsData = hangDroidPrefs.getInt("HIGH_SCORE_KEY", 0);
+
+        // assign the views from layout xml
+        TextView scoreText = (TextView)v.findViewById(R.id.scoresView);
+        TextView  pointsText = (TextView)v.findViewById(R.id.pointsView);
+
+        // set the views to display the high score and name
+        scoreText.setText(nameData);
+        pointsText.setText(String.valueOf(pointsData));
+
+
+        return v;
+
+
     }
 }
+
